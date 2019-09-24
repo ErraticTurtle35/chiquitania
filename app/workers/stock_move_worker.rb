af2e399ee9abb3b884268df84e8553518e802004
Gabriel Morales <gabriel.morales@swissbyted.ch>
2019-09-24 12:02:16 -0400

    Add worker for new stock.move

---
+++ app/workers/stock_move_worker.rb
@@ -0,0 +1,12 @@
+class StockMoveWorker
+    include Sneakers::Worker
+    from_queue "stock_move.post"
+
+    def work(message)
+        Sneakers.logger.info "message from the queue demo.post \n #{message}"
+        stock_move =  JSON.parse(message)
+        StockMove.create!(stock_move)
+        Sneakers.logger.info "message saved from the queue demo.post \n #{stock_move.to_json}"
+        ack!
+    end
+end
