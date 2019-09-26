312113a711712e0d357b46e031ef52b32e396951
Gabriel Morales <gabriel.morales@swissbyted.ch>
2019-09-25 21:25:58 -0400

    Changes to parser

--- app/workers/stock_move_worker.rb
+++ app/workers/stock_move_worker.rb
@@ -4,7 +4,7 @@ class StockMoveWorker
 
     def work(message)
         Sneakers.logger.info "message from the queue demo.post \n #{message}"
-        stock_move =  JSON.parse(message)
+        stock_move =  JSON.parse(message, :quirks_mode => true)
         StockMove.create!(stock_move)
         Sneakers.logger.info "message saved from the queue demo.post \n #{stock_move.to_json}"
         ack!
--- rabbitmq/send.rb
+++ rabbitmq/send.rb
@@ -16,7 +16,7 @@ move = {
     :date => '2019-09-23 08:44:58',
     :move_description => 'Ingreso',
     :uom_name => 'Unidades',
-    :quantity => 1.0,
+    :quantity => 4.0,
     :product_cost => 50.0
 }
 ch.default_exchange.publish(move.to_json, :routing_key => "stock_move.post", :content_type =>"application/json", :payload_encoding => "string")
